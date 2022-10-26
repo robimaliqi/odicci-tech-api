545e3260581fd2254733d9ed57d4b97595059b56
Robi <robi.maliqi@hotmail.co.uk>
2022-10-27 00:38:00 +0100

    updated the model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true, numericality: { only_integer: true }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP,
+    message: "must be valid email" }
+  validates :date_of_birth, presence: true, validate :dob_must_be_in_the_past
+
+
+  def dob_must_be_in_the_past
+    if Date.parse(date_of_birth) > Date.today
+      errors.add(:date_of_birth, "must be in the past")
+    end
+  end
+
 end
