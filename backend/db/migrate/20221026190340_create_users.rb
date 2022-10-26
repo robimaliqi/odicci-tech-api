5b00c86080312a4eed187b51b7c4b3c856a63e72
Robi <robi.maliqi@hotmail.co.uk>
2022-10-26 20:05:08 +0100

    added scaffold and schema

---
+++ backend/db/migrate/20221026190340_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.integer :phone_number
+      t.integer :date_of_birth
+
+      t.timestamps
+    end
+  end
+end
